a3eb4cf32c3335076652a2a6309f88255f675783
tomermoshe <tomosher1@gmail.com>
2019-02-13 16:02:26 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Angular-Cesium Template
---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { ViewerConfiguration } from 'angular-cesium';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [ViewerConfiguration],
+  encapsulation: ViewEncapsulation.None,
+})
+export class AppComponent {
+  constructor(private viewerConf: ViewerConfiguration) {
+    viewerConf.viewerOptions = {
+      selectionIndicator: false,
+      timeline: false,
+      infoBox: false,
+      baseLayerPicker: false,
+      animation: false,
+      shouldAnimate: false,
+      homeButton: false,
+      geocoder: false,
+      navigationHelpButton: false,
+      navigationInstructionsInitiallyVisible: false,
+    };
+
+    viewerConf.viewerModifier = (viewer: any) => {
+      viewer.screenSpaceEventHandler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
+      viewer.bottomContainer.remove();
+    };
+  }
+}
